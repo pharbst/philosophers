7407ece8bede5d3b4ed3dd25e54a14824ccdccd9
Peter Harbst <pharbst@student.42heilbronn.de>
2022-12-05 02:25:48 +0100

    headerfile created

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -5,21 +5,50 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/26 07:45:51 by pharbst           #+#    #+#             */
-/*   Updated: 2022/11/26 07:49:13 by pharbst          ###   ########.fr       */
+/*   Created: 2022/12/05 02:21:45 by pharbst           #+#    #+#             */
+/*   Updated: 2022/12/05 02:25:05 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <unistd.h>
+# include <stdlib.h>
 # include <stdio.h>
 # include <pthread.h>
 # include <sys/time.h>
 # include <string.h>
-# include "libftio.h"
-# include "philo_structs.h"
 
+typedef struct s_philo
+{
+	bool			alive;
+	int				id;
+	int				eat_count;
+	int				last_eat;
+	pthread_t		thread;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+}	t_philo;
 
+typedef struct s_a
+{
+	int				philo_count;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				eat_count;
+	int				start_time;
+	int				dead;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	write;
+	pthread_mutex_t	dead_mutex;
+	t_philo			*philo;
+}	t_a;
+
+int		ft_atoi(const char *str);
+int		ft_isdigit(int c);
+int		ft_strlen(const char *s);
 
 #endif
