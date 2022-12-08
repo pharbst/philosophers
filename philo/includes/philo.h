f922d95e512b811a92f500088f74aace9a859790
pharbst <peter.harbst@gmail.com>
2022-12-08 11:07:45 +0100

    added a struct and adjusted the main for it struct holds all parameters given by the user as const

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 02:21:45 by pharbst           #+#    #+#             */
-/*   Updated: 2022/12/05 07:13:52 by pharbst          ###   ########.fr       */
+/*   Updated: 2022/12/08 11:00:20 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,22 @@
 # include <string.h>
 # include <stdbool.h>
 
+typedef struct s_para
+{
+	const int		philo_count;
+	const int		time_to_die;
+	const int		time_to_eat;
+	const int		time_to_sleep;
+	const int		eat_count;
+}	t_para;
+
+
 typedef struct s_philo
 {
 	bool			alive;
 	int				id;
 	int				eat_count;
-	int				last_eat;
+	t_para			parameter;
 	pthread_t		thread;
 	pthread_mutex_t	*left_fork;
 	pthread_mutex_t	*right_fork;
@@ -35,13 +45,9 @@ typedef struct s_philo
 
 typedef struct s_a
 {
-	int				philo_count;
-	const int		time_to_die;
-	const int		time_to_eat;
-	const int		time_to_sleep;
-	int				eat_count;
 	int				start_time;
 	int				dead;
+	t_para			parameter;
 	pthread_mutex_t	*forks;
 	pthread_mutex_t	write;
 	pthread_mutex_t	dead_mutex;
@@ -50,5 +56,6 @@ typedef struct s_a
 
 long		philo_atoi(char *str);
 bool		execute_sim(t_a *a);
+void		*ft_calloc(size_t nelem, size_t elsize);
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,32 +6,60 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 02:26:25 by pharbst           #+#    #+#             */
-/*   Updated: 2022/12/05 07:37:10 by pharbst          ###   ########.fr       */
+/*   Updated: 2022/12/08 11:04:59 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
+#include "../includes/philo.h"
 
 static void	close_sim(t_a *a)
 {
-	printf("Simulation ended\n");
-	return (free(a));
+	int	i;
+
+	if (a->philo)
+		free(a->philo);
+	i = 0;
+	if (a->forks)
+		while (i < a->parameter.philo_count)
+			pthread_mutex_destroy(&a->forks[i++]);
+	if (a->forks)
+		free(a->forks);
+	return (printf("Simulation ended\n"), free(a));
+}
+
+static bool	init_simulation(t_a *a)
+{
+	int	i;
+
+	a->philo = ft_calloc(sizeof(t_philo), (a->parameter.philo_count + 2));
+	if (!a->philo)
+		return (true);
+	a->forks = ft_calloc(sizeof(pthread_mutex_t), (a->parameter.philo_count));
+	if (!a->forks)
+		return (true);
+	if (pthread_mutex_init(&a->write, NULL))
+		return (true);
+	i = 0;
+	while (i < a->parameter.philo_count)
+		if (pthread_mutex_init(&a->forks[i++], NULL))
+			return (true);
+	return (false);
 }
 
 t_a	*input_handler(int argc, char** argv)
 {
 	t_a	*a;
 
-	a = malloc(sizeof(t_a));
-	(*(int *)&a->philo_count) = philo_atoi(argv[1]);
-	(*(int *)&a->time_to_die) = philo_atoi(argv[2]);
-	(*(int *)&a->time_to_eat) = philo_atoi(argv[3]);
-	(*(int *)&a->time_to_sleep) = philo_atoi(argv[4]);
+	a = ft_calloc(sizeof(t_a), 1);
+	(*(int *)&a->parameter.philo_count) = philo_atoi(argv[1]);
+	(*(int *)&a->parameter.time_to_die) = philo_atoi(argv[2]);
+	(*(int *)&a->parameter.time_to_eat) = philo_atoi(argv[3]);
+	(*(int *)&a->parameter.time_to_sleep) = philo_atoi(argv[4]);
 	if (argc == 6)
-		a->eat_count = philo_atoi(argv[5]);
+		(*(int *)&a->parameter.eat_count) = philo_atoi(argv[5]);
 	else
-		a->eat_count = -1;
-	if (a->philo_count < 1 || a->time_to_die < 1 || a->time_to_eat < 1 || a->time_to_sleep < 1 || (argc == 6 && a->eat_count < 1))
+		(*(int *)&a->parameter.eat_count) = -1;
+	if (a->parameter.philo_count < 1 || a->parameter.time_to_die < 1 || a->parameter.time_to_eat < 1 || a->parameter.time_to_sleep < 1 || (argc == 6 && a->parameter.eat_count < 1))
 		return (write(1, "Error: invalid input\n", 21), NULL);
 	return (a);
 }
@@ -45,7 +73,9 @@ int main(int argc, char **argv)
 	a = input_handler(argc, argv);
 	if (!a)
 		return (1);
+	if (init_simulation(a))
+		return (close_sim(a), write(2, "Error: initialization failed\n", 29), 2);
 	if (execute_sim(a))
-		return (close_sim(a), printf("philo died\n"), 1);
+		return (close_sim(a), printf("philo died\n"), 0);
 	return (close_sim(a), 0);
 }
