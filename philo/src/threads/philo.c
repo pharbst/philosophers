96e729a80e91106e21b2869709f787a71c7f4480
pharbst <peter.harbst@gmail.com>
2023-01-03 00:36:30 +0100

    fork functions cleaned up and removed some mistakes

--- philo/src/threads/philo.c
+++ philo/src/threads/philo.c
@@ -6,57 +6,35 @@
 /*   By: peter <peter@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 12:22:01 by pharbst           #+#    #+#             */
-/*   Updated: 2023/01/02 23:45:19 by peter            ###   ########.fr       */
+/*   Updated: 2023/01/03 00:35:43 by peter            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
-
-// static int	philo_sleep(unsigned int wakeuptime, unsigned long deathtime)
-// {
-// 	unsigned long	actualtime;
-// 	struct timeval	tv;
-
-// 	if (wakeuptime > deathtime)
-// 		wakeuptime = deathtime;
-// 	gettimeofday(&tv, NULL);
-// 	actualtime = tv.tv_sec * 1000000 + tv.tv_usec;
-// 	while(actualtime < wakeuptime)
-// 	{
-// 		usleep(1);
-// 		gettimeofday(&tv, NULL);
-// 		actualtime = tv.tv_sec * 1000000 + tv.tv_usec;
-// 	}
-// 	if (wakeuptime == deathtime)
-// 		return (1);
-// 	return (0);
-// }
 
 bool	take_fork2(t_philo philo)
 {
 	if (philo.id % 2 == 0)
 	{
 		pthread_mutex_lock(philo.m_run);
-		if (*(philo.run) == true)
-		{
-			pthread_mutex_lock(philo.left_fork);
-			printf("==%lu==	philo%d took left fork\n", timestamp(philo.parameter.starttime));
-			pthread_mutex_lock(philo.m_run);
-		}
-		else
+		if (*(philo.run) == false)
 			return (true);
+		pthread_mutex_lock(philo.left_fork);
+		pthread_mutex_lock(&philo.m_id);
+		printf("==%lu==	philo%d took left fork\n", timestamp(philo.parameter.starttime), philo.id);
+		pthread_mutex_unlock(&philo.m_id);
+		pthread_mutex_lock(philo.m_run);
 	}
 	else
 	{
 		pthread_mutex_lock(philo.m_run);
-		if (*(philo.run) == true)
-		{
-			pthread_mutex_lock(philo.right_fork);
-			printf("==%lu==	philo%d took right fork\n", timestamp(philo.parameter.starttime));
-			pthread_mutex_lock(philo.m_run);
-		}
-		else
+		if (*(philo.run) == false)
 			return (true);
+		pthread_mutex_lock(philo.right_fork);
+		pthread_mutex_lock(&philo.m_id);
+		printf("==%lu==	philo%d took right fork\n", timestamp(philo.parameter.starttime), philo.id);
+		pthread_mutex_unlock(&philo.m_id);
+		pthread_mutex_lock(philo.m_run);
 	}
 	return (false);
 }
@@ -69,20 +47,21 @@ bool	take_fork1(t_philo philo)
 		if (*(philo.run) == false)
 			return (true);
 		pthread_mutex_lock(philo.right_fork);
-		printf("==%lu==	philo%d took right fork\n", timestamp(philo.parameter.starttime));
+		pthread_mutex_lock(&philo.m_id);
+		printf("==%lu==	philo%d took right fork\n", timestamp(philo.parameter.starttime), philo.id);
+		pthread_mutex_unlock(&philo.m_id);
 		pthread_mutex_unlock(philo.m_run);
 	}
 	else
 	{
 		pthread_mutex_lock(philo.m_run);
-		if (*(philo.run) == true)
-		{
-			pthread_mutex_lock(philo.left_fork);
-			printf("==%lu==	philo%d took left fork\n", timestamp(philo.parameter.starttime));
-			pthread_mutex_unlock(philo.m_run);
-		}
-		else
+		if (*(philo.run) == false)
 			return (true);
+		pthread_mutex_lock(philo.left_fork);
+		pthread_mutex_lock(&philo.m_id);
+		printf("==%lu==	philo%d took left fork\n", timestamp(philo.parameter.starttime), philo.id);
+		pthread_mutex_unlock(&philo.m_id);
+		pthread_mutex_unlock(philo.m_run);
 	}
 	return (take_fork2(philo));
 }
@@ -96,7 +75,7 @@ void	philo(t_philo philo)
 		printf("==%lu==	philo%d is thinking", timestamp(philo.parameter.starttime), philo.id);
 		pthread_mutex_unlock(&philo.m_id);
 		pthread_mutex_unlock(philo.m_run);
-		if (take_fork1 == true)
+		if (take_fork1(philo) == true)
 			return ;
 		pthread_mutex_lock(philo.m_run);
 		if (*(philo.run) == false)
