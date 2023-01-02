0946b0c7ddb2ac5eec4d023b2f4f29dcd2500533
pharbst <peter.harbst@gmail.com>
2023-01-03 00:34:27 +0100

    fixed a mistake in the time function and a mistake in the sim executer

--- philo/src/helping_functions/time.c
+++ philo/src/helping_functions/time.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   timestamp.c                                        :+:      :+:    :+:   */
+/*   time.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
+/*   By: peter <peter@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 10:53:27 by pharbst           #+#    #+#             */
-/*   Updated: 2022/12/09 07:16:12 by pharbst          ###   ########.fr       */
+/*   Updated: 2023/01/03 00:24:15 by peter            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,5 @@ unsigned long   timestamp(unsigned long starttime)
     struct timeval  tv;
 
     gettimeofday(&tv, NULL);
-    return ((tv.tv_sec * 1000000 + tv.tv_usec) - starttime / 1000);
+    return (((tv.tv_sec * 1000000 + tv.tv_usec) - starttime) / 1000);
 }
--- philo/src/simualtion_setup/execute_sim.c
+++ philo/src/simualtion_setup/execute_sim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute_sim.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
+/*   By: peter <peter@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 07:06:17 by pharbst           #+#    #+#             */
-/*   Updated: 2022/12/09 09:19:19 by pharbst          ###   ########.fr       */
+/*   Updated: 2023/01/03 00:32:10 by peter            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,13 @@ bool	execute_sim(t_a *a)
 	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
-	(*(unsigned long *)a->parameter.starttime) = tv.tv_sec * 1000000 + tv.tv_usec;
-	printf("==%lu==	", timestamp(a->parameter.starttime));
+	(*(unsigned long *)&a->parameter.starttime) = tv.tv_sec * 1000000 + tv.tv_usec;
+	printf("==%lu==	\n", timestamp(a->parameter.starttime));
+	printf("sleepfunction test wait for %d ms\n", a->parameter.time_to_eat);
+	real_usleep(utime() + a->parameter.time_to_eat * 1000);
+	printf("==%lu==	\n", timestamp(a->parameter.starttime));
+	printf("input values\n");
 	printf("Simulation started with %d philosopers %dms needed to eat %dms needed to sleep %dms to die, each philo should eat %d times\n", a->parameter.philo_count, a->parameter.time_to_eat, a->parameter.time_to_sleep, a->parameter.time_to_die, a->parameter.eat_count);
+	printf("atm no simulation\n");
 	return (false);
 }
