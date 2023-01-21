aab4d805fe457093966410602f8283e85188ad48
Peter Harbst <pharbst@student.42heilbronn.de>
2023-01-21 06:20:28 +0100

    organized functions in catagorys and prepared the header file for the new philo routine (function prototypes are commented)

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
+/*   By: pharbst <pharbst@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 02:21:45 by pharbst           #+#    #+#             */
-/*   Updated: 2023/01/20 01:48:11 by pharbst          ###   ########.fr       */
+/*   Updated: 2023/01/21 06:18:42 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,16 +61,16 @@ typedef struct s_a
 	pthread_t		thread;
 }	t_a;
 
+
+
+/*general helper functions*/
+
 /*philo_atoi is for converting input str in a number valid for the project philo*/
 long			philo_atoi(char *str);
-/*execute_sim starts the execution of the simulation*/
-bool			execute_sim(t_a *a);
-/*ft_calloc allocate memory and wipe it with 0*/
-void			*ft_calloc(size_t nelem, size_t elsize);
 /*ft_bzero wipe memory with 0*/
 void			ft_bzero(void *s, size_t n);
-/*philo is the main function for all philosophers*/
-void			*philo_main(void *philo);
+/*ft_calloc allocate memory and wipe it with 0*/
+void			*ft_calloc(size_t nelem, size_t elsize);
 /*utime returns the values of gettimeofday together as a unsigned long in usec*/
 unsigned long   utime();
 /*timestamp returns the diffrenence of starttime and actual time in ms*/
@@ -78,7 +78,29 @@ unsigned long   timestamp(unsigned long starttime);
 /*real_usleep is not a real sleep function is is a function waiting for the wakeuptime in the format of utime*/
 void			real_usleep(unsigned long wakeuptime);
 
+
+
+/*general managing functions*/
+
+/*execute_sim starts the execution of the simulation*/
+bool			execute_sim(t_a *a);
+
+
+
+/*thread routine functions*/
+
+/*philo is the main function for all philosophers*/
+void			*philo_main(void *philo);
+/*vitalmonitor monitors the deathtime of all philos and set the run variable to false if a philo dies*/
 void			*vitalmonitor(void *a);
 
+
+
+// /*thread routine helper functions*/
+
+// /*locks the run mutex and prints the log massage if the run variable is true*/
+// bool			print_log(t_philo *philo, char *str);
+// /*fork taking function for philo routine*/
+// bool			take_fork(t_philo *philo);
 
 #endif
